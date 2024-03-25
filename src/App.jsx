2c1f8d9c69898af07ead2b499622c06cc3a33287
Elias Alban <Alban001@users.noreply.github.com>
2024-03-25 16:06:27 -0400

    primera subida

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './styles/container.css'
+
+function App() {
+  
+
+  return (
+    <div className='container'>
+        <header className='header-container'>
+          <nav className='shift'>
+            <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Acerca de mi trabajo</a></li>
+            <li><a href="">Fotografias</a></li>
+            <li><a href="">Contactos</a></li></ul>
+          </nav>
+        </header>
+    </div>
+  )
+}
+
+export default App
