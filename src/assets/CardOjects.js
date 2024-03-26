e87828cde2406d09402a14d6a8ec36a7db14dbd5
Elias Alban <Alban001@users.noreply.github.com>
2024-03-26 09:36:07 -0400

    imagenes removidas

--- src/assets/CardOjects.js
+++ src/assets/CardOjects.js
@@ -1,20 +1,20 @@
 const object = [{
     id:1,
-    img:'src/img/card1.jpg',
+    img:'/card1.jpg',
     desc: 'Innovación',
-    insta:'src/img/insta.png',
+    insta:'/insta.png',
 },
 {
     id:2,
-    img:'src/img/card2.jpg',
+    img:'/card2.jpg',
     desc: 'Memorias',
-    wats:'src/img/whatsapp.png',
+    wats:'/whatsapp.png',
 },
 {
     id:3,
-    img:'src/img/card3.jpg',
+    img:'/card3.jpg',
     desc: 'Inspiracion',
-    face:'src/img/facebook.png',
+    face:'/facebook.png',
 },
 ]
 
