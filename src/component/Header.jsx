99d37efa74104fe2ea4eff2367efcfe5b8cd76f4
Elias Alban <Alban001@users.noreply.github.com>
2024-03-28 19:23:24 -0400

    agregado navbar hamburgesa

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/headerContainer.css'
 
 const Header = () => {
+  const [click, setClick] = useState(false)
+
+  const handleClick =()=>{
+    setClick(!click)
+    console.log(click)
+  }
   return (
-    <header className='header-container'>
-      <h1>TU LOGO</h1>
-        <input className="side-menu" type="checkbox" id="side-menu"/>
-        <label className="hamb" htmlFor="side-menu"><span className="hamb-line"></span></label>
-    <nav className='nav stroke'>
-      <ul className='menu'>
-         <li><a href="#home">Home</a></li>
-         <li><a href="#arte">Trabajo & Arte</a></li>
-         <li><a href="#aboutMe">Sobre mi</a></li>
-         <li><a href="#contact">Contactos</a></li>
-      </ul>
-    </nav>
+    <div className='header-container'>
+    <div className='logo'><h1>tu logo</h1></div>
+    <header className={click ? "is-open" : ""}>
+      <nav className="stroke"> 
+         <ul >
+             <li><a href="#home">Home</a></li>
+             <li><a href="#arte">Trabajo & Arte</a> </li>
+             <li><a href="#aboutMe">Sobre mi</a></li>
+             <li><a href="#contact">Contactos</a></li>
+         </ul>
+     </nav>
+  
   </header>
+  <li className='trigger' onClick={handleClick}>Menu</li>
+    </div>
+    
   )
 }
 
