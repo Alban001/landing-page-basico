2f565554623d6d8dc9afbd160e20382bbaafba94
Elias Alban <Alban001@users.noreply.github.com>
2024-03-28 14:02:48 -0400

    mobile responsive fixed

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,12 +7,13 @@ const Header = () => {
       <h1>TU LOGO</h1>
         <input className="side-menu" type="checkbox" id="side-menu"/>
         <label className="hamb" htmlFor="side-menu"><span className="hamb-line"></span></label>
-    <nav className='stroke' id='nav-id'>
+    <nav className='nav stroke'>
       <ul className='menu'>
-      <li><a href="#home">Home</a></li>
-      <li><a href="#arte">Trabajo & Arte</a></li>
-      <li><a href="#aboutMe">Sobre mi</a></li>
-      <li><a href="#contact">Contactos</a></li></ul>
+         <li><a href="#home">Home</a></li>
+         <li><a href="#arte">Trabajo & Arte</a></li>
+         <li><a href="#aboutMe">Sobre mi</a></li>
+         <li><a href="#contact">Contactos</a></li>
+      </ul>
     </nav>
   </header>
   )
